19da5a8df2684179ee93c2dcba7052aa22b93958
Dmitry Andreev <dmigand@gmail.com>
2020-11-21 17:19:36 +0300

    Initial commit

---
+++ Assets/Scripts/PoolServiceLocator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asteroids
+{
+    public static class PoolServiceLocator
+    {
+        private static readonly Dictionary<Type, object> _poolContainer = new Dictionary<Type, object>();
+
+        public static void SetPool<T>(T pool) where T : IPool
+        {
+            // не понял, зачем тут нужна проверка на наличие в словаре ключа? если нет - добавит, если есть - передобавит
+            // или это из соображений производительности?
+            _poolContainer[typeof(T)] = pool;
+        }
+
+        public static T Resolve<T>()
+        {
+            // переделал твою реализацию проверки наличия ключа на TryGetValue
+            // вроде бы под капотом делает то же самое
+            _poolContainer.TryGetValue(typeof(T), out var pool);
+            return (T) pool;
+        }
+    }
+}
